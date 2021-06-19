83f468344021c5d368157b7b8a3e4cdf46b29dc8
Archana Badagi <82663278+archanabadagi@users.noreply.github.com>
2021-06-19 15:52:09 +0530

    Rename next_greatest_same_digits.cpp to 12_next_greatest_same_digits.cpp

--- APS_Code_Library/12_next_greatest_same_digits.cpp
+++ APS_Code_Library/12_next_greatest_same_digits.cpp
@@ -1,6 +1,6 @@
 /*
   Given a number n, find the smallest number that has same set of digits as n and is greater than n.
-  If n is the greatest possible number with its set of digits, then print “not possible”.
+  If n is the greatest possible number with its set of digits, then print â€œnot possibleâ€.
 */
 #include <iostream>
 #include <cstring>
